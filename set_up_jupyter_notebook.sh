b125f9b726dee2b7090f573ec1a9a20feaa7fa9c
Yu Lou <louyu27@uw.edu>
2019-12-19 00:22:24 +0000

    remove ununsed file

--- set_up_jupyter_notebook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Initialize configuration file if needed
-jupyter notebook --generate-config && \
-# Set a password
-jupyter notebook password
-# Start notebook
-jupyter notebook --no-browser
